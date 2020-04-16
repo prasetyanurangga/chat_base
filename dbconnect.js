4337f54cf2ccd4bf003863ba95284427953b23b9
Angga Nur Prasetya <35420062+prasetyanurangga@users.noreply.github.com>
2020-04-16 21:40:58 +0700

    Update dbconnect.js

--- dbconnect.js
+++ dbconnect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.Promise = require("bluebird");
 
-const url = "mongodb://localhost:27017/chat";
+const url = "mongodb+srv://syntinen:rahasia@cluster0-2v386.mongodb.net/chat";
 
 const connect = mongoose.connect(url, { useNewUrlParser: true });
 
